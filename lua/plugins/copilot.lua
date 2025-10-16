06346e0cb4073347c1c191fabd748e17b783a73c
lukin921 <lukin921@student.liu.se>
2025-10-16 21:02:50 +0200

    added render markdown plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,15 +6,13 @@ return {
     },
     build = 'make tiktoken',
     opts = {
-      {
-        model = 'gpt-4.1',
-        temperature = 0.1, -- Lower = focused, higher = creative
-        window = {
-          layout = 'vertical', -- 'vertical', 'horizontal', 'float'
-          width = 0.5, -- 50% of screen width
-        },
-        auto_insert_mode = true,
+      model = 'gpt-4.1',
+      temperature = 0.1, -- Lower = focused, higher = creative
+      window = {
+        layout = 'vertical', -- 'vertical', 'horizontal', 'float'
+        width = 0.5, -- 50% of screen width
       },
+      auto_insert_mode = true,
     },
     keys = {
       {
@@ -26,5 +24,15 @@ return {
         mode = 'n',
       },
     },
+    config = function()
+      require('render-markdown').setup {
+        file_types = { 'markdown', 'copilot-chat' },
+      }
+      require('CopilotChat').setup {
+        highlight_headers = false,
+        separator = '---',
+        error_header = '> [!ERROR] Error',
+      }
+    end,
   },
 }
